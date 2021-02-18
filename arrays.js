3149c915ea7e661a4584b38d7178c8c861ac20ac
Dev Frias <ddfrias14@gmail.com>
2021-02-18 20:55:26 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,5 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
-function addElementToBeginningOfArray(element, array){
+function addElementToBeginningOfArray(array, element){
   var moreChocolateBars = [element + ...chocolateBars];
   return moreChocolateBars;
 }
